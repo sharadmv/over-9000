57535962c165abfc03596545be82d45d00304d83
Siddhartho Bhattacharya <sid_gogol@hotmail.com>
2012-10-12 22:45:55 -0700

    blob detection on stream

--- cv/python/imageHandler.py
+++ cv/python/imageHandler.py
@@ -1,34 +1,51 @@
 from SimpleCV import *
 import time
 
-cam = Camera()
-
 CAMERA_PROPERTIES = {'width':160, 'height':120}
+cam = Camera(prop_set=CAMERA_PROPERTIES)
 
-def process_frame(frame):
-    frame.show()
+def process_frame(image):
+	#image.show()
+	img = image.edges(100,880)
+	#img.show()
+	blobs = img.findBlobs()
+	if blobs:
+		b = blobs[-1]
+		b.show()
+		print(b.area)
+		print(b.centroid)
+
+	#biggestHull = max([(b,b.area()) for b in blobs],key=lambda x:x[1])
+	#biggestHull[0].show()
+	#for b in blobs:
+	#	time.sleep(1)
+	#	b.show()	
+	#	print(b.area)
 
 def main():
-    cam = Camera(prop_set=CAMERA_PROPERTIES)
+    #cam = Camera(prop_set=CAMERA_PROPERTIES)
     while True:
         f = cam.getImage()
         process_frame(f)
 
-def imageHandler(image):
-	img = Image(image);
-	return
 	
 
 def imageShower():
-	img = cam.getImage()
+	img = cam.getImage();
 	img.show()
 	time.sleep(1)
-	img = img.edges(100,880)
-	img.show()
+	ed = img.edges(100,880)
+	ed.show()
 	blobs = img.findBlobs()
 	#biggestHull = max([(b,b.area()) for b in blobs],key=lambda x:x[1])
 	#biggestHull[0].show()
-	for b in blobs:
-		time.sleep(1)
-		b.show()	
-		print(b.area)
+	#for b in blobs:
+	#	time.sleep(1)
+	#	b.show()	
+	#	print(b.area)
+	b = blobs[-1]
+	b.show()
+	print(b.area)
+	print(b.centroid)
+
+main()
