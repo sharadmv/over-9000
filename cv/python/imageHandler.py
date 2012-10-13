249264d05b78c2c76523439d310639ba4c55c532
Siddhartho Bhattacharya <sid_gogol@hotmail.com>
2012-10-12 22:23:11 -0700

    beginnings of blob tracking

--- cv/python/imageHandler.py
+++ cv/python/imageHandler.py
@@ -1,3 +1,27 @@
+from SimpleCV import *
+import time
+
+cam = Camera()
 
 def imageHandler(image):
+	img = Image(image);
+	return
 	
+
+def imageShower():
+	img = cam.getImage()
+	img.show()
+	time.sleep(1)
+	img = img.edges(100,880)
+	img.show()
+	blobs = img.findBlobs()
+	#biggestHull = max([(b,b.area()) for b in blobs],key=lambda x:x[1])
+	#biggestHull[0].show()
+	for b in blobs:
+		time.sleep(1)
+		b.show()	
+		print(b.area)
+		
+
+imageShower()
+		
