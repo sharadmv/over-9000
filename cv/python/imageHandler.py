8ba783821754822330acf51a3e034698f54b5629
Sharad Vikram <sharad.vikram@gmail.com>
2012-10-12 22:19:26 -0700

    got simplecv working

--- cv/python/imageHandler.py
+++ cv/python/imageHandler.py
@@ -1,3 +1,12 @@
+from SimpleCV import *
 
-def imageHandler(image):
-	
+CAMERA_PROPERTIES = {'width':160, 'height':120}
+
+def process_frame(frame):
+    frame.show()
+
+if __name__ == 'main':
+    cam = Camera(prop_set=CAMERA_PROPERTIES)
+    while True:
+        f = cam.getImage()
+        process_frame(f)
