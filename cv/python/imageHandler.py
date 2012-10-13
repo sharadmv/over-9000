2f9acc1485a13b4179a4379f7b32c9b87537dd55
Sharad Vikram <bousheesnaw@gmail.com>
2012-10-12 22:23:43 -0700

    fixed main glitch

--- cv/python/imageHandler.py
+++ cv/python/imageHandler.py
@@ -5,8 +5,10 @@ CAMERA_PROPERTIES = {'width':160, 'height':120}
 def process_frame(frame):
     frame.show()
 
-if __name__ == 'main':
+def main():
     cam = Camera(prop_set=CAMERA_PROPERTIES)
     while True:
         f = cam.getImage()
         process_frame(f)
+
+main()
