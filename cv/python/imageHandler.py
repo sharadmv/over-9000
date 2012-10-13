54ac2be2f5fb05798e1bdf9e9cc2b4cd62110c6c
Sharad Vikram <bousheesnaw@gmail.com>
2012-10-13 13:32:01 -0700

    update gesture

--- cv/python/imageHandler.py
+++ cv/python/imageHandler.py
@@ -3,10 +3,11 @@ from SimpleCV import *
 import time
 import urllib2 as url
 import os
+import socket
 
 from urllib import urlretrieve
 
-CAMERA_PROPERTIES = {'width':160, 'height':120}
+CAMERA_PROPERTIES = {'width':80, 'height':60}
 
 class HandExtractor:
 
@@ -26,13 +27,14 @@ class HandExtractor:
         while True:
             f = self.camera.getImage()
             self.f = f.copy()
-	    self.f.show()
+            self.f.show()
             hand = self.process_frame(f)
-            self.process_hand(hand)
-            gesture = self.get_gesture()
-            if gesture:
-                self.trigger_gesture(gesture)
-            time.sleep(0.01)
+            if hand:
+                self.process_hand(hand)
+                gesture = self.get_gesture()
+                if gesture:
+                    self.trigger_gesture(gesture)
+                    #time.sleep(0.02)
 
     def get_gesture(self):
         if self.count >= 7:
@@ -78,50 +80,59 @@ class HandExtractor:
                 self.current_direction = None
 
     def process_frame(self, image):
-		segment = HaarCascade("face.xml")
-		face = image.findHaarFeatures(segment)
-	
-		image = image.toHSV()
-		for i in range(160):
-			for j in range(120):
-				h,s,v = image[i,j]
-				if v>= 15 and v<=250 and h>=3 and h<=33:
-					image[i,j] = (100,0,0)
-				else:
-					image[i,j] = (0,0,100)
-					
-		image = image.toRGB()
-		blobs = image.findBlobs()
-		ed = image.edges(60,300)
-		
+        #segment = HaarCascade("face.xml")
+        #face = image.findHaarFeatures(segment)
 
-		if blobs:
-			for b in blobs:
-				b.drawHull(color=Color.GREEN,width=3,alpha=128)
-				
-			x,y = blobs[-1].centroid()
-			circleLayer = DrawingLayer((image.width, image.height))
-			circleLayer.circle((x,y), 20, Color.RED, True)
-			image.addDrawingLayer(circleLayer)
-			image.applyLayers()
-			#image.show()
-			
-		return (x,y)
+       #for j in xrange(120):
+       #    for i in xrange(160):
+       #        h,s,v = image[i,j]
+       #        if v>= 15 and v<=250 and h>=3 and h<=33:
+       #            image[i,j] = (100,0,0)
+       #        else:
+       #            image[i,j] = (0,0,100)
+
+        image = image.getSkintoneMask(0)
+        blobs = image.findBlobs()
+        if blobs:
+            x,y = blobs[-1].centroid()
+            """
+            for b in blobs:
+                b.drawHull(color=Color.GREEN,width=3,alpha=128)
+
+            circleLayer = DrawingLayer((image.width, image.height))
+            circleLayer.circle((x,y), 20, Color.RED, True)
+            image.addDrawingLayer(circleLayer)
+            image.applyLayers()
+            image.show()
+            """
+            return (x,y)
 
     def trigger_gesture(self, gesture):
         #url.urlopen('http://localhost:8080/api/'+gesture)
         print("YES A GESTURE:", gesture)
+        self.hit_hai(gesture)
         self.take_picture()
 
     def take_picture(self):
         print("YAY TAKING PICTURE")
         self.f.save('output.jpg')
         print("DONE")
-        os.system("curl -F 'access_token=AAACEdEose0cBAF25xs7EFGJ7HPY4nJjtu1bNcq7IOlI2zyAg81ZBqQY1NLIzPZAIWhv4iCYQNwFnvRMWTyNvkf66RZAcC8UqmOyLb2cA9ZCUPX2lFMF9' -F 'source=@output.jpg' https://graph.facebook.com/me/photos")
+        os.system("curl -F 'access_token=AAACEdEose0cBABOjbLZB6N8rsHiqu0mkO6oYNnxyockNxIqEW1GRwnBj6G9ZBLBcLxSrRjatJ8WtA3NI40ECrKbltZAtTuFM02EL7DhFXkgCcaeafzU' -F 'source=@output.jpg' https://graph.facebook.com/me/photos")
 
 
     def plot(self):
         print(self.buffer)
+
+    def hit_hai(self, gesture):
+        print("HITTING HAI NAO")
+        HOST = '10.42.0.1'    # The remote host
+        PORT = 4444# The same port as used by the server
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("CONNECTING TO HAI")
+        s.connect((HOST, PORT))
+        print("CONNECTED TO HAI")
+        s.send(gesture)
+        s.close()
 
 def main():
     hand = HandExtractor()
